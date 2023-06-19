fb81d722fb85bb31c662f01467bf6822884baf62
voidfearxy <135716980+voidfearxy@users.noreply.github.com>
2023-06-19 19:38:55 +0300

    Update pkdğ.py

--- pkdğ.py
+++ pkdğ.py
@@ -1,12 +1,14 @@
 from telegram.ext import Updater, CommandHandler
+from telegram import Bot
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Merhaba!")
 
 def main():
     # Telegram botunuzun tokenını buraya girin
-    token = '6181887397:AAE1MKVuEiX8ce_QLBiT4N37iQO7h5Gqgrg'
-    updater = Updater(token=token, use_context=True)
+    token = 'YOUR_BOT_TOKEN'
+    bot = Bot(token=token)
+    updater = Updater(bot=bot, use_context=True)
     dispatcher = updater.dispatcher
 
     start_handler = CommandHandler('start', start)
